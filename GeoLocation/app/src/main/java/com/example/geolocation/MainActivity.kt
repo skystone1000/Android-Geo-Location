2a39173f814ff2d3fe68a0d5a4550027a3267683
Aditya <adityaspmahajan@gmail.com>
2023-11-29 11:09:05 +0530

    App 2 - Basic Geo Location Implementation

--- GeoLocation/app/src/main/java/com/example/geolocation/MainActivity.kt
+++ GeoLocation/app/src/main/java/com/example/geolocation/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.geolocation
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
 
 class MainActivity : AppCompatActivity() {
+
+    // Step 2.0 - Variables
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Step 2.1 - Initialise Var
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // Step 2.2 - Check permissions
+        // checkLocationPermission()
+
+        // Step 3.0 - Create View in XML
+        findViewById<Button>(R.id.btn_get_location).setOnClickListener(){
+            checkLocationPermission()
+        }
+    }
+
+    private fun checkLocationPermission() {
+        val task = fusedLocationProviderClient.lastLocation
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            &&
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
+            return
+        }
+
+        task.addOnSuccessListener {
+            if(it != null){
+                Toast.makeText(applicationContext, "${it.latitude} ${it.longitude}", Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
