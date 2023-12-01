5fee1f97a91a372dd53600490dca557945029d45
Aditya <adityaspmahajan@gmail.com>
2023-12-01 14:20:59 +0530

    Implementation 2 - Restructure

--- GeoLocation/app/src/main/java/com/example/geolocation/MainActivity.kt
+++ GeoLocation/app/src/main/java/com/example/geolocation/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.geolocation
 
+import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.tasks.Task
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,34 +20,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Step 2.1 - Initialise Var
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        // New activity
+        // https://stackoverflow.com/questions/4186021/how-to-start-new-activity-on-button-click
+        // https://stackoverflow.com/questions/44301301/android-how-to-achieve-setonclicklistener-in-kotlin
 
-        // Step 2.2 - Check permissions
-        // checkLocationPermission()
+        val button1 = findViewById<View>(R.id.btn_implementation1) as Button
+        val button2 = findViewById<View>(R.id.btn_implementation2) as Button
+        val button3 = findViewById<View>(R.id.btn_implementation3) as Button
 
-        // Step 3.0 - Create View in XML
-        findViewById<Button>(R.id.btn_get_location).setOnClickListener(){
-            checkLocationPermission()
-        }
-    }
-
-    private fun checkLocationPermission() {
-        val task = fusedLocationProviderClient.lastLocation
-
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            &&
-            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-        ){
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
-            return
-        }
-
-        task.addOnSuccessListener {
-            if(it != null){
-                Toast.makeText(applicationContext, "${it.latitude} ${it.longitude}", Toast.LENGTH_LONG).show()
+        val listener= View.OnClickListener { view ->
+            when (view.id) {
+                R.id.btn_implementation1 -> {
+                    val intent = Intent(this, LocationActivity2::class.java)
+                    startActivity(intent)
+                }
+                R.id.btn_implementation1 -> {
+                    // Do some work here
+                }
+                R.id.btn_implementation1 -> {
+                    // Do some work here
+                }
             }
         }
+
+        button1.setOnClickListener(listener)
+        button2.setOnClickListener(listener)
+        button3.setOnClickListener(listener)
 
     }
 }
